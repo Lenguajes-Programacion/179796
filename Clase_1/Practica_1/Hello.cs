c7ea1fa2762c770222d739f60128fce804086b1f
Giovanna Noriega <60111523+GiovannaN18@users.noreply.github.com>
2020-01-24 11:21:30 -0700

    Practica_1

---
+++ Clase_1/Practica_1/Hello.cs
@@ -0,0 +1,15 @@
+using System;
+// Importamos la libreria principal.
+
+namespace practica_1
+{
+    class Program
+    {
+        static void Main(string args)
+        {
+            Console.WriteLine("Hola Mundo!");
+        }
+    }
+}
+    
+
