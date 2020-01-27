fab792226a378f89ab7fe821de555a6798c6a837
Giovanna Noriega <60111523+GiovannaN18@users.noreply.github.com>
2020-01-27 12:32:57 -0700

    Condiciones

---
+++ Tareas/Practica_uno/Practica_uno/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Practica_uno
+{
+    class Program
+    {
+
+        static string nombre = "Giovanna";
+        static void Main(string[] args)
+        {
+            //Respondemos con el primer argumento y segundo.
+            //Condicion se debe cumplir todas las sentencias si utilizamos &&
+            //Condicion se debe cumplir todas las sentencias si utilizamos ||
+            //Tipos de Comparacion <> == <= >= !=(diferente que)
+            if (args[0].Length > 0 || args[0] == nombre) //true or false/booleano
+            {
+                Console.WriteLine($"Hello {args[0]} {args[1]}");
+            }
+            else
+            {
+                Console.WriteLine("User unauthorized");
+            }
+        }
+    }
+}
